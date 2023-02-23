8ed3571e6e445ebcd222e634e8cd20850867acd1
Suellen Xavier <sxcwork@gmail.com>
2023-02-23 18:33:49 -0300

    alteracao

--- app/validacao.js
+++ app/validacao.js
@@ -3,7 +3,7 @@ function verificaChute(chute){
     const numero = +chute
 
     if(chuteInvalido(numero)){
-        elementoChute.innerHTML = `Você disse: ${chute}. Valor inválido!`
+        elementoChute.innerHTML = `Você disse: ${chute}.Valor inválido!`
     }
 
     if(numeroInvalido(numero)){
